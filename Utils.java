78f6c08f9678d82b81ec1e4dc1d94bf2de9e33a7
Alexandre Borgo <alexandre.borgo@free.fr>
2018-11-22 11:43:07 +0100

    check fakecoin 2 last octets at 0

--- Utils.java
+++ Utils.java
@@ -1,10 +1,22 @@
 public class Utils {
 
+    /* Transform an array of bytes into a String in hexadecimal */
     public static String bytesToString(byte[] bytes) {
         String result = "";
         for(byte b : bytes) {
             result += String.format("%02x", b);
         }
         return result;
+    }
+
+    /* Return true if the given hash is a Fakecoin hash (two last octets at 0) or false if it is not */
+    public static boolean isFakecoinHash(String hash) {        
+        if( hash.charAt(hash.length()-1) == '0' && 
+            hash.charAt(hash.length()-2) == '0' &&
+            hash.charAt(hash.length()-3) == '0' &&
+            hash.charAt(hash.length()-4) == '0') {
+            return true;
+        }
+        return false;
     }
 }
